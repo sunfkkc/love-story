be0e7772b04818766eab9ab7685fa6cb1aefa501
sunfkkc <rlcks77@nate.com>
2023-04-14 15:57:22 +0900

    feat: SecurityConfig

---
+++ server/src/main/java/lovestory/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package lovestory.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import lovestory.domain.role.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint().userService(customOAuth2UserService).and().and().build();
+    }
+}
