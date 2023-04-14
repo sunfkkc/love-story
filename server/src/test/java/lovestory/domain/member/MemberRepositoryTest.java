4e14be4f934b7c4bd317c9d2d93e1183f3dac6fc
sunfkkc <rlcks77@nate.com>
2023-04-14 11:20:38 +0900

    test: 멤버 등록 테스트 추가

---
+++ server/src/test/java/lovestory/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package lovestory.domain.member;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 멤버_등록(){
+        String name = "김기찬";
+        String email = "rlcks77@nate.com";
+        LocalDateTime now = LocalDateTime.now();
+
+        memberRepository.save(Member.builder().name(name).email(email).build());
+
+        List<Member> members = memberRepository.findAll();
+
+        Member member = members.get(0);
+
+        assertThat(member.getName()).isEqualTo(name);
+        assertThat(member.getEmail()).isEqualTo(email);
+
+        assertThat(member.getCreatedDate()).isAfter(now);
+        assertThat(member.getModifiedDate()).isAfter(now);
+    }
+}
