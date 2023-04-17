c8f47a3d4bf0fa003453aeb521a955fa52672f97
sunfkkc <rlcks77@nate.com>
2023-04-17 17:46:59 +0900

    chore: 환경변수를 불러와 사용할수 있게 설정

---
+++ server/src/main/java/lovestory/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package lovestory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:/application-oauth.properties")
+})
+public class PropertyConfig {
+}
