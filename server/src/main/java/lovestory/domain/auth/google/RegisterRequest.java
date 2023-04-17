04634e98a9f6dd74f04e2e24e2a6addc2dde500e
sunfkkc <rlcks77@nate.com>
2023-04-17 19:16:51 +0900

    update: token값에 validator 추가

--- server/src/main/java/lovestory/domain/auth/google/RegisterRequest.java
+++ server/src/main/java/lovestory/domain/auth/google/RegisterRequest.java
@@ -1,11 +1,14 @@
 package lovestory.domain.auth.google;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank
     private String token;
 
 
