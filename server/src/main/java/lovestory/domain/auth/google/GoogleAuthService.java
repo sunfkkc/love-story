b9f1d8defacbbf76db3f8b1b4455987917c413ce
sunfkkc <rlcks77@nate.com>
2023-04-17 19:17:58 +0900

    feat: 구글 api를 이용하여 토큰값이 유효한지 판단하는 verifyToken 메서드 생성

---
+++ server/src/main/java/lovestory/domain/auth/google/GoogleAuthService.java
@@ -0,0 +1,71 @@
+package lovestory.domain.auth.google;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import lovestory.domain.member.Member;
+import lovestory.domain.role.Role;
+
+
+@Service
+public class GoogleAuthService {
+    @Value("${google-client-id}")
+    private String CLIENT_ID;
+
+    private final NetHttpTransport transport = new NetHttpTransport();
+    private final JsonFactory jsonFactory = new GsonFactory();
+
+
+
+    public Member verifyToken(String token) {
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                .build();
+        try {
+            GoogleIdToken idToken = verifier.verify(token);
+            if (idToken != null) {
+                Payload payload = idToken.getPayload();
+
+                // Print user identifier
+                String userId = payload.getSubject();
+                System.out.println("User ID: " + userId);
+
+                // Get profile information from payload
+                String email = payload.getEmail();
+                boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                String locale = (String) payload.get("locale");
+                String familyName = (String) payload.get("family_name");
+                String givenName = (String) payload.get("given_name");
+
+                Member member = Member.builder().name(name).email(email).picture(pictureUrl).role(Role.USER).build();
+                return member;
+
+
+            } else {
+                System.out.println("Invalid ID token.");
+
+            }
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Member();
+    }
+
+
+
+}
