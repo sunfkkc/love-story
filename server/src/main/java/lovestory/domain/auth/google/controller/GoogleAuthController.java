0d09bb579d0215ae5ea3fcf8a0846ac2df7dca89
sunfkkc <rlcks77@nate.com>
2023-04-17 19:18:24 +0900

    update: 사용자의 토큰 값이 유효하면 구글 회원 정보를 리턴

--- server/src/main/java/lovestory/domain/auth/google/controller/GoogleAuthController.java
+++ server/src/main/java/lovestory/domain/auth/google/controller/GoogleAuthController.java
@@ -1,14 +1,34 @@
 package lovestory.domain.auth.google.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+import lovestory.config.ErrorResponse;
+import lovestory.domain.auth.google.GoogleAuthService;
 import lovestory.domain.auth.google.RegisterRequest;
+import lovestory.domain.member.Member;
+
 @RestController
+@RequiredArgsConstructor
 public class GoogleAuthController {
+    private final GoogleAuthService googleAuthService;
+    private final HttpSession httpSession;
+
     @PostMapping("/api/v1/auth/google")
-    public String member(@RequestBody RegisterRequest request){
-        return request.getToken();
+    public ResponseEntity<Object> member(@RequestBody @Valid RegisterRequest request, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("invalid token"));
+        }
+        Member member = googleAuthService.verifyToken(request.getToken());
+        return ResponseEntity.status(HttpStatus.OK).body(member);
+
     }
 }
