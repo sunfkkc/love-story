4bec103ff57bd16696cbb9dc85b5ce7f8e634b0b
sunfkkc <rlcks77@nate.com>
2023-04-18 17:12:59 +0900

    update: MemberRepository에 findByEmail 메서드 추가

--- server/src/main/java/lovestory/domain/member/MemberRepository.java
+++ server/src/main/java/lovestory/domain/member/MemberRepository.java
@@ -2,5 +2,8 @@ package lovestory.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
 }
