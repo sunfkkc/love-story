225b904bc4612497908e14996050a44a67db41c6
sunfkkc <rlcks77@nate.com>
2023-04-22 20:45:55 +0900

    feat: PartnershipRepository

---
+++ server/src/main/java/lovestory/domain/member/PartnershipRepository.java
@@ -0,0 +1,6 @@
+package lovestory.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartnershipRepository extends JpaRepository<Partnership,Long> {
+}
