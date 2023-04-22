b24045a75c158c5d78e3c5d65d4fa782babe9e27
sunfkkc <rlcks77@nate.com>
2023-04-22 20:17:42 +0900

    feat: 에러 응답 객체 생성

---
+++ server/src/main/java/lovestory/domain/ErrorResponse.java
@@ -0,0 +1,13 @@
+package lovestory.domain;
+
+public class ErrorResponse {
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
