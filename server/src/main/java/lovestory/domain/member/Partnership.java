489f64cc3b40b89934261a9ab43d00130e7bc79c
sunfkkc <rlcks77@nate.com>
2023-04-22 20:45:44 +0900

    fix: 카멜케이스 제거 및 id 타입 Long으로 변경

--- server/src/main/java/lovestory/domain/member/Partnership.java
+++ server/src/main/java/lovestory/domain/member/Partnership.java
@@ -5,13 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lovestory.domain.BaseTimeEntity;
-
+@Getter
+@NoArgsConstructor
 @Entity
-public class PartnerShip extends BaseTimeEntity {
+public class Partnership extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
 
 }
