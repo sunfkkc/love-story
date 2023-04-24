0ca1b4691d4e8cede783fabd1ba3231ed2b399fe
sunfkkc <rlcks77@nate.com>
2023-04-24 15:40:17 +0900

    refactor: 구글 로그인 훅 삭제

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,10 +1,7 @@
-import { useGoogleLogin } from '@/hooks/login/useGoogleLogin';
 import React from 'react';
 
 const LoginPage = () => {
-  const { mutate, data } = useGoogleLogin();
-  console.log(data);
-  return <button onClick={() => mutate()}>구글 로그인</button>;
+  return <div></div>;
 };
 
 export default LoginPage;
