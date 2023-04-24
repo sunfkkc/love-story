fcf5ae54f6d4ba1ff19eba2f4b062f61247ee5c4
sunfkkc <rlcks77@nate.com>
2023-04-24 15:56:31 +0900

    chore: 프로젝트 기본 설정

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={new QueryClient()}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
